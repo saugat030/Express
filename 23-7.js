3862c1bf3e30998e24489d5cdf43b6b81d8ff7c5
ZuluMumu <saugatdahal030@gmail.com>
2024-11-27 15:11:08 +0545

    first node mini projects

--- 23-7.js
+++ 23-7.js
@@ -1,2 +1,27 @@
 import inquirer from "inquirer";
-console.log("He;l;p");
+import qr from "qr-image";
+import fs from "fs";
+//var qr = require("qr-image");
+
+console.log("Helpppppppppp");
+
+inquirer
+  .prompt([{ message: "Type the URL:", name: "URL" }])
+  .then((answers) => {
+    const url = answers.URL;
+
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("./23-7static/qr_image.png"));
+
+    fs.writeFile("./23-7static/URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("File has been saved.");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
