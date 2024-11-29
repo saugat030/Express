a3f57f0a6d785d76dcd8bd905d77c536cc4137b5
ZuluMumu <saugatdahal030@gmail.com>
2024-11-29 15:37:16 +0545

    woo

---
+++ MiddleWares/index1.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+//Very imp module
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true })); //body parser makes req.body available. Natra undefined hunthiyo
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send("Submit requested");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
