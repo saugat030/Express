21cb36c1cf174035129e9aed2e7fc840449422e5
ZuluMumu <saugatdahal030@gmail.com>
2024-12-04 21:20:58 +0545

    CustomApi

---
+++ APIs/APIProj/5.6 Secrets Project/index.js
@@ -0,0 +1,21 @@
+// HINTS:
+// 1. Import express and axios
+import express from "express";
+import axios from "axios";
+app.use(express.static("public"));
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
