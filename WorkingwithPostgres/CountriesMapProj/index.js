7e62125dfa5766fd6e3ff4c2205075d4912dced8
ZuluMumu <saugatdahal030@gmail.com>
2024-12-12 21:52:55 +0545

    Added exception handeling

--- WorkingwithPostgres/CountriesMapProj/index.js
+++ WorkingwithPostgres/CountriesMapProj/index.js
@@ -19,19 +19,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  const result = await db.query("select * from visited_countries");
+  const result = await db.query("select * from visited_countries"); //could do a select country_code from visited_countries.
   let arr = [];
   if (result.rows.length > 0) {
-    //result.rows will alaways be an array and never null or undefunes. so it will always exist. Btter to check with .length
+    //result.rows will always be an array and never null or undefine. so it will always exist. Btter to check with .length
     result.rows.forEach((item) => {
       arr.push(item.country_code);
     });
     res.render("index.ejs", { countries: arr, total: arr.length });
   } else {
-    res.render("index.ejs", { error: "Not found." });
+    res.render("index.ejs", { error: "No data in the array" });
+    db.end();
   }
 });
-
+app.post();
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
