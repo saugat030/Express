b76bdc50c7596fdd3ca12aa8b606200954d8d5e6
ZuluMumu <saugatdahal030@gmail.com>
2024-12-15 19:17:23 +0545

    making the get req fetch the data from the database.

--- WorkingwithPostgres/ToDO/index.js
+++ WorkingwithPostgres/ToDO/index.js
@@ -3,20 +3,35 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "todo",
+  password: "12345678",
+  port: 5432,
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+let items = [];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM items");
+    if (result.rows.length > 0) {
+      items = result.rows;
+      res.render("index.ejs", {
+        listTitle: "Today",
+        listItems: items,
+      });
+    } else {
+      console.log("No data found.");
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.redirect("/");
+  }
 });
 
 app.post("/add", (req, res) => {
